9b2aa5acde5bef6eb7119554cbcb4e59fa6dc0d5
Cole Phillips <colephillips1@gmail.com>
2023-05-25 15:42:49 -0400

    Add password generation functionality

--- index.js
+++ index.js
@@ -1,3 +1,112 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const passwordOne = document.getElementById("password-1");
+const passwordTwo = document.getElementById("password-2");
+const generatePassword = document.getElementById("genpassword")
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
+
+generatePassword.addEventListener("click", function() {
+	passwordOne.textContent = ""; // ensure old passwords are cleared
+  passwordTwo.textContent = ""; 
+	for (let i = 0; i < 11; i++) {
+    randomIndexOne = characters[Math.floor(Math.random() * characters.length)];
+    randomIndexTwo = characters[Math.floor(Math.random() * characters.length)];
+		passwordOne.textContent += randomIndexOne
+		passwordTwo.textContent += randomIndexTwo
+  }
+})
+
+
+// document.getElementById("password-1").style.textAlign = "center";
+// document.getElementById("password-2").style.textAlign = "center";
